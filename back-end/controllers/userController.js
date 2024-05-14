f09c0409b53b34a1aa5aefb3b2425d4a22e04b1e
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-05-14 10:48:58 +0530

    updated

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -12,8 +12,8 @@ const userController = {
                     return res.status(400).json({msg:"Please Login or Registers"})
                 }
 
-                jwt.verify(rf_token,process.env.REFRESH_TOKEN_SECRET,(err,user) => {
-                    if(err){
+                jwt.verify(rf_token,process.env.REFRESH_TOKEN_SECRET,(error,user) => {
+                    if(error){
                         return res.status(400).json({msg:"Please Login or Register"})   
                     }
                     
@@ -22,8 +22,8 @@ const userController = {
                 })
 
             }
-            catch(err){
-                return res.status(500).json({msg:err.message})
+            catch(error){
+                return res.status(500).json({msg:error.message})
             }
     },
 
@@ -43,9 +43,7 @@ const userController = {
                 password: hashedPassword
             });
 
-            console.log('Hashed password:', hashedPassword);
             await newUser.save();
-
             res.status(201).json({ message: 'User registered successfully' });
         }
         catch(error){
@@ -55,19 +53,19 @@ const userController = {
     },
 
     login: async(req,res) => {
-        try {
+        try{
             const { email, password } = req.body;
 
             const user = await Users.findOne({ email });
-            if (!user) {
+            if(!user){
                 return res.status(400).json({ msg: "User does not exist" });
             }
 
-            if (!password) {
+            if(!password){
                 return res.status(400).json({ msg: "Password is required" });
             }
 
-            if (!user.password) {
+            if(!user.password){
                 return res.status(500).json({ msg: "User password not found" });
             }
 
@@ -89,30 +87,6 @@ const userController = {
         catch(error){
             console.error(error)
             return res.status(500).json({msg:error.message});
-        }
-    },
-
-    updateUser: async (req, res) => {
-        try {
-            /*const { id } = req.params; // Get the user ID from request parameters
-            const { role } = req.user; // Get the role attribute of the logged-in user
-
-            // If user is not an admin, return an error
-            if (role !== 1) {
-                return res.status(403).json({ msg: "Only admins can update user profiles" });
-            }
-
-            // If an admin, proceed with updating the user profile*/
-            const updatedUser = await Users.findByIdAndUpdate(id, req.body, { new: true });
-
-            if (!updatedUser) {
-                return res.status(404).json({ msg: "User not found" });
-            }
-
-            res.json({ msg: "User profile updated successfully", user: updatedUser });
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({ msg: "Internal server error" });
         }
     },
 
@@ -142,7 +116,24 @@ const userController = {
         catch(err){
             return res.status(500).json({msg:err.message})
         }
-    }
+    },
+
+    updateUser: async (req, res) => {
+        try{
+            const { id } = req.params;
+            const updatedUser = await Users.findByIdAndUpdate(id, req.body, { new: true });
+
+            if(!updatedUser){
+                return res.status(404).json({ msg: "User not found" });
+            }
+
+            res.json({ msg: "User profile updated successfully", user: updatedUser });
+        }
+        catch(error){
+            console.error(error);
+            return res.status(500).json({ msg: "Internal server error" });
+        }
+    },
 }
 
 
