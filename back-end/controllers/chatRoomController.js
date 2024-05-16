9b15f6904e979ffad1787892a8861b2c1fa85aab
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-05-16 21:57:33 +0530

    updated

--- back-end/controllers/chatRoomController.js
+++ back-end/controllers/chatRoomController.js
@@ -5,33 +5,33 @@ const User = require('../models/userModel');
 const chatRoomController = {
     createChatRoom: async(req,res) => {
         try{
-            const loggedInUser = req.user; // Assuming req.user is populated by your authentication middleware
+            const loggedInUser = req.user;
             if (!loggedInUser) {
                 return res.status(401).json({ msg: 'User not authenticated' });
             }
 
             const { name, members } = req.body;
 
-            // Add the logged-in user as the admin of the chat room
+            
             const chatRoom = new ChatRoom({
                 name,
                 members: members || [],
-                admin: loggedInUser._id, // Assigning the logged-in user as the admin
+                admin: loggedInUser._id, 
             });
 
-            // Save the chat room
+          
             await chatRoom.save();
 
-    // Create a separate collection for users in the chat room
+    
             const chatRoomMembers = new ChatRoomMembers({
                 chatRoomId: chatRoom._id,
                 members: [loggedInUser._id, ...(members || [])],
             });
 
-    // Save the chat room members collection
+   
             await chatRoomMembers.save();
 
-    // Assign temporary role of cr-admin to the user
+    
             loggedInUser.role = 'cr-admin';
             await loggedInUser.save();
 
