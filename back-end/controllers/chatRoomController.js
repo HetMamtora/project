0960f6a08ed0db4a8a36b10f7ed55ae32dc3537c
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-05-24 15:05:42 +0530

    updated

--- back-end/controllers/chatRoomController.js
+++ back-end/controllers/chatRoomController.js
@@ -100,6 +100,82 @@ const chatRoomController = {
             console.error("Error message:", error.message);
             res.status(500).send('Server Error');
         }
+    },
+
+    removeSingleMember: async(req,res) => {
+        try{
+            const loggedInUser = req.user;
+            const { chatRoomId, memberId } = req.params;
+
+            // Find the chat room
+            const chatRoom = await ChatRoom.findById(chatRoomId);
+            if (!chatRoom) {
+                return res.status(404).json({ msg: 'Chat room not found' });
+            }
+
+             // Find the user's role in the chat room
+            const chatRoomMembers = await ChatRoomMembers.findOne({ chatRoomId });
+            if (!chatRoomMembers) {
+                return res.status(404).json({ msg: 'Chat room members not found' });
+            }
+
+            const userRole = chatRoomMembers.members.find(
+                (member) => member.userId.toString() === loggedInUser._id.toString()
+            );
+
+            if (!userRole || userRole.role !== 'cr-admin') {
+                return res.status(403).json({ msg: 'User is not authorized to remove members from this chat room' });
+            }
+
+            // Remove the member
+            chatRoomMembers.members = chatRoomMembers.members.filter(
+                (member) => member.userId.toString() !== memberId
+            );
+
+            await chatRoomMembers.save();
+
+            chatRoom.members = chatRoom.members.filter(
+                (member) => member.toString() !== memberId
+              );
+              await chatRoom.save();
+
+            res.json({ msg: 'Member removed from the chat room' });
+        }
+        catch(error){
+
+        }
+    },
+
+    leaveChatRoom: async(req,res) => {
+        try {
+            const loggedInUser = req.user;
+            const { chatRoomId } = req.params;
+        
+            // Find the chat room members
+            const chatRoomMembers = await ChatRoomMembers.findOne({ chatRoomId });
+            if (!chatRoomMembers) {
+                return res.status(404).json({ msg: 'Chat room members not found' });
+            }
+        
+            // Remove the logged-in member from chatRoomMembers
+            chatRoomMembers.members = chatRoomMembers.members.filter(
+                (member) => member.userId.toString() !== loggedInUser._id.toString()
+            );
+            await chatRoomMembers.save();
+        
+            // Remove the logged-in member from chatRoom
+            const chatRoom = await ChatRoom.findById(chatRoomId);
+            chatRoom.members = chatRoom.members.filter(
+                (member) => member.toString() !== loggedInUser._id.toString()
+            );
+            await chatRoom.save();
+        
+            res.json({ msg: 'You have left the chat room' });
+          }
+          catch (error) {
+                console.error("Error message:", error.message);
+                res.status(500).send('Server Error');
+          }
     }
 }
 
--- back-end/routes/chatRoomRoutes.js
+++ back-end/routes/chatRoomRoutes.js
@@ -4,6 +4,7 @@ const chatRoomController = require('../controllers/chatRoomController')
 const auth = require('../middleware/auth')
 
 router.post('/createchatroom' ,auth,chatRoomController.createChatRoom)
-router.delete('/deletechatroom/:chatRoomId', auth, chatRoomController.closeChatRoom);
+router.delete('/deletechatroom/:chatRoomId', auth, chatRoomController.closeChatRoom);   
+router.delete('/:chatRoomId/members/:memberId', auth, chatRoomController.removeSingleMember);
 
 module.exports = router
