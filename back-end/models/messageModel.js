10dce74f196c8a4d8292b3af6afb00335bd23729
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-05-30 18:08:25 +0530

    updated

---
+++ back-end/models/messageModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    chatRoomId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ChatRoom',
+        required: true
+    },
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    message: {
+        type: 
+    }
+})
